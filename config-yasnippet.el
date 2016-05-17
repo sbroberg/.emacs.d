1a657725372b097b5ccf3930f4a407fd1554b4e5
Stephen Broberg <sbroberg@carbonite.com>
2016-05-17 16:00:08 -0400

    Removed unnecessary load

--- config-yasnippet.el
+++ config-yasnippet.el
@@ -2,8 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-to-list 'load-path
-              "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
 (define-key yas-keymap (kbd "<return>") 'yas/exit-all-snippets)
