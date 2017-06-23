46cd05d65384ad49f8784177bafc3f6f7f1170fc
Stephen Broberg <sbroberg@carbonite.com>
2017-06-23 15:47:52 -0400

    Fixed my iedit mode to use the built-in defun limiter instead of using narr-to-defun, which is more destructive

--- config-iedit.el
+++ config-iedit.el
@@ -12,16 +12,7 @@
   (interactive "P")
   (if arg
       (iedit-mode)
-    (save-excursion
-      (save-restriction
-        (widen)
-        ;; this function determines the scope of `iedit-start'.
-        (if iedit-mode
-            (iedit-done)
-          ;; `current-word' can of course be replaced by other
-          ;; functions.
-          (narrow-to-defun)
-          (iedit-start (current-word) (point-min) (point-max)))))))
+    (iedit-mode 0)))
 
 (global-set-key (kbd "C-;") 'iedit-dwim)
 
