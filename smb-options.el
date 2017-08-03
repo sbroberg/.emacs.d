8821a9c6c019d215afc94b3c70eded22db0f41d0
Stephen Broberg <sbroberg@carbonite.com>
2017-08-03 15:42:55 -0400

    Changes for Windows

--- smb-options.el
+++ smb-options.el
@@ -78,4 +78,9 @@
 ;; (load-theme 'material t) ;; load material theme
 ;; (global-linum-mode t) ;; enable line numbers globally
 
+;; needed to suppress stupid "unsafe directory" messages when
+;; using magit
+(require 'server)
+ (and (>= emacs-major-version 23)
+     (defun server-ensure-safe-dir (dir) "Noop" t))
 ;;; smb-options ends here
