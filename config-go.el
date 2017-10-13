a05bc6e101e8f6a6c24389389552b3db61a37f8f
Stephen Broberg <sbroberg@carbonite.com>
2017-10-13 09:14:44 -0400

    Refinements to go-mode

--- config-go.el
+++ config-go.el
@@ -3,6 +3,8 @@
 ;;; Code:
 
 (require 'eshell)
+(require 'go-mode)
+
 (load-relative "./go-guru")
 
 (defun set-exec-path-from-shell-PATH ()
@@ -22,7 +24,7 @@
 
 (defun my-go-mode-hook ()
   "Enable go mode features."
-  ; Use goimports instead of go-fmt
+  ; Use goimports instead of gofmt
   (setq gofmt-command "goimports")
   ; Call Gofmt before saving
   (add-hook 'before-save-hook 'gofmt-before-save)
@@ -30,9 +32,11 @@
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
            "go build -v && go test -v && go vet"))
-  ; Godef jump key binding
+  ; uniform jump key binding
   (local-set-key (kbd "M-.") 'godef-jump)
-  (local-set-key (kbd "M-*") 'pop-tag-mark)
+  (local-set-key (kbd "<s-left>") 'pop-tag-mark)
+  (local-set-key (kbd "M-m") 'go-guru-expand-region)
+  (local-set-key (kbd "M-,") 'go-guru-callers)
 )
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
